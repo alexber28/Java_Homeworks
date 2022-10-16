d227fe5604f26ab2fd4033391ba685f6828b0d73
alexber28 <88576201+alexber28@users.noreply.github.com>
2022-10-16 17:43:41 +0300

    Add files via upload

---
+++ Hw18.10_Opearators/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //_Exercise_1____________
+        int number = 3214;
+        System.out.println(number % 10);
+
+        //_Exercise_2____________
+        //int number = 5713;
+        //System.out.println((number / 100) + (number % 100));
+
+        //_Exercise_3____________
+        //int number = 15290;
+        //System.out.println(number / 100 % 10);
+
+        //_Exercise_4____________
+        //int firstPointX = 1, firstPointY = 1;
+        //int secondPointX = 4, secondPointY = 5;
+        //double length = Math.sqrt(Math.pow(firstPointX - secondPointX, 2) + Math.pow(firstPointY - secondPointY, 2));
+        //System.out.println(length);
+
+        //_Exercise_5____________
+        //double a = 6, b = 8;
+        //double r = 4.99;
+        //double r0 = Math.sqrt(Math.pow(a/2, 2) + Math.pow(b/2, 2));
+        //if (r >= r0)
+        //    System.out.println("Yes");
+        //else
+        //    System.out.println("No");
+
+        //_Exercise_6___y = k * x + b - straight line equation
+        //int firstPointX = 2, firstPointY = 3;
+        //int secondPointX = 4, secondPointY = -1;
+        //double k = (double) (firstPointY - secondPointY) / (firstPointX - secondPointX);
+        //double b = (double) secondPointX*secondPointY/(firstPointX - secondPointX) - (double) secondPointX*firstPointY/(firstPointX - secondPointX) + secondPointY;
+        //System.out.println("Straight line equation: y = " + k + " * x + " + b);
+    }
+}
