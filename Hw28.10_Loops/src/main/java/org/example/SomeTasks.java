f34f1d813f17bbba6b0568033efcf980bcd8ac8a
alexber28 <88576201+alexber28@users.noreply.github.com>
2022-10-28 16:02:19 +0300

    Update SomeTasks.java

--- Hw28.10_Loops/src/main/java/org/example/SomeTasks.java
+++ Hw28.10_Loops/src/main/java/org/example/SomeTasks.java
@@ -1,19 +1,17 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class SomeTasks {
     public static void main(String[] args) {
-//        Random random = new Random();
-//        int[] testArray = new int[10];
-//
-//        for (int i = 0; i < testArray.length; i++) {
-//            testArray[i] = random.nextInt(100) - 49;
-//        }
-//        System.out.println("Array:\n" + Arrays.toString(testArray));
+        //For tasks 1-9
+        Random random = new Random();
+        int[] testArray = new int[10];
+
+        for (int i = 0; i < testArray.length; i++) {
+            testArray[i] = random.nextInt(100) - 49;
+        }
+        System.out.println("Array:\n" + Arrays.toString(testArray));
 
         //Task1,2
 //        int maximum = testArray[0];
@@ -120,9 +118,154 @@ public class SomeTasks {
 //
 //        System.out.println("Result array: " + taskArray);
 
-        //Task10
-        
+//        //Task10
+//        int[][] matrix = new int[5][5];
+//
+//        Random random = new Random();
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                matrix[i][j] = random.nextInt(20) - 9;
+//            }
+//        }
+//        System.out.println("Matrix: \n" + Arrays.deepToString(matrix));
+//
+//        int maxSum = matrix[0][0] + matrix[0][1] + matrix[0][2] + matrix[0][3] + matrix[0][4];
+//
+//        for (int i = 1; i < matrix.length; i++) {
+//            int sum = 0;
+//
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                sum += matrix[i][j];
+//            }
+//
+//            if (sum > maxSum) {
+//                maxSum = sum;
+//            }
+//        }
+//        System.out.println("Maximal sum of lines is: " + maxSum);
 
+        //Task11
+//        int[][] matrix = new int[4][4];
+//        Random random = new Random();
+//
+//        System.out.println("Matrix:");
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                matrix[i][j] = random.nextInt(20) - 9;
+//                System.out.print(matrix[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = i + 1; j < matrix[i].length; j++) {
+//                int temp = matrix[i][j];
+//                matrix[i][j] = matrix[j][i];
+//                matrix[j][i] = temp;
+//            }
+//        }
+//
+//        System.out.println("\nTransposed matrix:");
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        //Task12
+//        int[][] matrix = new int[4][4];
+//        int temp;
+//        boolean condition = false;
+//        Random random = new Random();
+//
+//        System.out.println("Matrix:");
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                matrix[i][j] = random.nextInt(20) - 9;
+//                System.out.print(matrix[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                if (matrix.length % 2 == 1 && i == matrix.length / 2 && j == matrix[i].length / 2) {
+//                    condition = true;
+//                    break;
+//                }
+//                if (matrix.length % 2 == 0 && i == matrix.length / 2) {
+//                    condition = true;
+//                    break;
+//                }
+//                temp = matrix[i][j];
+//                matrix[i][j] = matrix[matrix.length - i - 1][matrix[i].length - j - 1];
+//                matrix[matrix.length - i - 1][matrix[i].length - j - 1] = temp;
+//            }
+//            if (condition) {
+//                break;
+//            }
+//        }
+//
+//        System.out.println("\nConverted matrix:");
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        //Task13
+//        int length, summary = 0, numberOfElements = 0, sumForElements = 0;
+//        double arithmeticMean;
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter size of matrix: ");
+//        length = sc.nextInt();
+//
+//        int[][] matrix = new int[length][length];
+//        Random random = new Random();
+//
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                if (i == j || i + j == length - 1) {
+//                    matrix[i][j] = 0;
+//                }
+//                if ((j > i && i + j < length - 1) || (i > j && i + j >= length)) {
+//                    matrix[i][j] = random.nextInt(10);
+//                }
+//                if ((j > i && i + j >= length) || (i > j && i + j < length - 1)) {
+//                    matrix[i][j] = random.nextInt(10) - 10;
+//                }
+//            }
+//        }
+//
+//        System.out.println("Matrix:");
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                summary += matrix[i][j];
+//            }
+//        }
+//        System.out.println("Summary of all elements: " + summary);
+//
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                if (matrix[i][j] > summary) {
+//                    sumForElements += matrix[i][j];
+//                    numberOfElements++;
+//                }
+//            }
+//        }
+//        arithmeticMean = (double) sumForElements / numberOfElements;
+//        System.out.println("Arithmetic mean: " + arithmeticMean);
+//
     }
 
     private static int sumOfDigits(int number) {
